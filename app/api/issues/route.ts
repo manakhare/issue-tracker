e3ebee9a27d7e721a07ba92706706a13e539f19c
Mana Khare <manakhare5@gmail.com>
2024-02-26 17:48:46 +0530

    Handle errors in NewIssuePage

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -4,7 +4,7 @@ import prisma from "@/prisma/client";
 
 // for data validation
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
+    title: z.string().min(1, 'Title is required').max(255),
     description: z.string().min(1),
 })
 
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
     const validation = createIssueSchema.safeParse(body);
 
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400 });
+        return NextResponse.json(validation.error.format(), { status: 400 });
 
     //insert new issue in db
     const newIssue = await prisma.issue.create({
